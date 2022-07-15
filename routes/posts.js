9e7bf7fc5d2a1b957145c72c8cc3ca6158357d0e
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-15 16:36:56 +0100

    added create and read functions

--- routes/posts.js
+++ routes/posts.js
@@ -2,28 +2,40 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
-router.get("/post1", (req, res) => {
-  res.send("This is post 1");
-});
-
-router.post("/", (req, res) => {
+// create a post
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
 
-  post
-    .save()
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
+
+// find a post
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// delete a post
 
 module.exports = router;
